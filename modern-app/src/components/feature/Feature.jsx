3a85ce165f3d627cd6a29025f27d2d949247a54f
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-10-20 17:15:13 +0530

    Feat: Added footer & css to app

--- modern-app/src/components/feature/Feature.jsx
+++ modern-app/src/components/feature/Feature.jsx
@@ -4,14 +4,14 @@ import './feature.css';
 const Feature = ({ text, title }) => {
   return (
     <div className="gpt3__features-container__feature">
-    <div className="gpt3__features-container__feature-title">
-      <div />
-      <h1>{title}</h1>
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
     </div>
-    <div className="gpt3__features-container_feature-text">
-      <p>{text}</p>
-    </div>
-  </div>
   );
 };
 
